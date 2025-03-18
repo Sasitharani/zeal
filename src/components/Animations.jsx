6bd189608852f926ee7475cd6dbf922c42cfe895
sasi <sasitharani@gmail.com>
2025-03-18 17:54:56 +0530

    Add tailwindcss-animate plugin and define custom animations in Tailwind config

--- src/components/Animations.jsx
+++ src/components/Animations.jsx
@@ -3,68 +3,123 @@ import 'tailwindcss/tailwind.css';
 
 const Animations = () => {
     return (
-        <nav className="flex justify-center space-x-4 p-4 bg-gray-800 text-white ">
-            <a
-                href="#"
-                className="hover:animate-bounce px-4 py-2 rounded"
-            >
-                Button Shake
-            </a>
-            <a
-                href="#"
-                className="hover:border-2 hover:border-blue-500 px-4 py-2 rounded border border-transparent"
-            >
-                Border Animation
-            </a>
-            <a
-                href="#"
-                className="hover:text-yellow-500 px-4 py-2 rounded"
-            >
-                Text Color Change
-            </a>
-            <a
-                href="#"
-                className="hover:shadow-lg hover:shadow-blue-500 px-4 py-2 rounded"
-            >
-                Shadow Effect
-            </a>
-            <a
-                href="#"
-                className="hover:rotate-6 transform px-4 py-2 rounded"
-            >
-                Rotate
-            </a>
-            <a
-                href="#"
-                className="hover:bg-blue-500 px-4 py-2 rounded"
-            >
-                Change Background Color
-            </a>
-            <a
-                href="#"
-                className="hover:animate-bounce px-4 py-2 rounded"
-            >
-                Bounce
-            </a>
-            <a
-                href="#"
-                className="hover:opacity-50 px-4 py-2 rounded"
-            >
-                Fade In/Out
-            </a>
-            <a
-                href="#"
-                className="hover:animate-pulse px-4 py-2 rounded"
-            >
-                Pulse Effect
-            </a>
-            <a
-                href="#"
-                className="hover:bg-gradient-to-r hover:from-blue-500 hover:to-green-500 px-4 py-2 rounded"
-            >
-                Gradient Background Animation
-            </a>
-        </nav>
+        <div className="h-screen overflow-y-scroll mt-16 pb-16">
+            <div className="flex flex-col items-center p-4 bg-gradient-to-br from-cyan-700 via-cyan-800 via-cyan-600 via-blue-500 via-gray-700 to-cyan-900 rounded-2xl">
+                <h1 className="text-2xl font-bold text-white mb-4">
+                    Hover to see the animation
+                </h1>
+                <div className="grid grid-cols-3 gap-4 text-white">
+                    <div className="border border-white p-4 rounded hover:animate-bounce">
+                        Button Shake
+                    </div>
+                    <div className="border border-white p-4 rounded hover:border-blue-500">
+                        Border Animation
+                    </div>
+                    <div className="border border-white p-4 rounded hover:text-yellow-500">
+                        Text Color Change
+                    </div>
+                    <div className="border border-white p-4 rounded hover:shadow-lg hover:shadow-blue-500">
+                        Shadow Effect
+                    </div>
+                    <div className="border border-white p-4 rounded hover:rotate-6 transform">
+                        Rotate
+                    </div>
+                    <div className="border border-white p-4 rounded hover:bg-blue-500">
+                        Change Background Color
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-bounce">
+                        Bounce
+                    </div>
+                    <div className="border border-white p-4 rounded hover:opacity-50">
+                        Fade In/Out
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-pulse">
+                        Pulse Effect
+                    </div>
+                    <div className="border border-white p-4 rounded hover:bg-gradient-to-r hover:from-blue-500 hover:to-green-500">
+                        Gradient Background Animation
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-fadeInLeft">
+                        Fade In Left
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-fadeInRight">
+                        Fade In Right
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-fadeInUp">
+                        Fade In Up
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-fadeOut">
+                        Fade Out
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-fadeOutDown">
+                        Fade Out Down
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-fadeOutLeft">
+                        Fade Out Left
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-fadeOutRight">
+                        Fade Out Right
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-fadeOutUp">
+                        Fade Out Up
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-slideInDown">
+                        Slide In Down
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-slideInLeft">
+                        Slide In Left
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-slideInRight">
+                        Slide In Right
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-slideInUp">
+                        Slide In Up
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-slideOutDown">
+                        Slide Out Down
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-slideOutLeft">
+                        Slide Out Left
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-slideOutRight">
+                        Slide Out Right
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-slideOutUp">
+                        Slide Out Up
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-slideDown">
+                        Slide Down
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-slideLeft">
+                        Slide Left
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-slideRight">
+                        Slide Right
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-slideUp">
+                        Slide Up
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-zoomIn">
+                        Zoom In
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-zoomOut">
+                        Zoom Out
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-tada">
+                        Tada
+                    </div>
+                    <div className="border border-white p-4 rounded hover:animate-spinnerGrow">
+                        Spinner Grow
+                    </div>
+                    <div className="relative">
+                        <button className="relative z-10 px-6 py-3 text-white bg-blue-500 rounded-full border-2 animate-serial-light">
+                            Animated Button
+                        </button>
+                    </div>
+                    <div className="relative w-32 h-32 rounded-full border-4 animate-serial-light">asd</div>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,157 @@
-export default {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        customBlue: '#214a5e',
-        customBlueLight1: '#3b6b7a', // Add this line for the first lighter version
-        customBlueLight2: '#558b97', // Add this line for the second lighter version
-      },
-      animation: {
-        shake: 'shake 0.5s ease-in-out infinite',
-        bounce: 'bounce 1s infinite', // Ensure bounce animation is enabled
-      },
-      keyframes: {
-        shake: {
-          '0%, 100%': { transform: 'translateX(0)' },
-          '50%': { transform: 'translateX(-5px)' },
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+    content: [
+        './src/**/*.{js,jsx,ts,tsx}', // Adjust paths as needed
+    ],
+    theme: {
+        extend: {
+            colors: {
+                customBlue: '#214a5e',
+                customBlueLight1: '#3b6b7a', // Add this line for the first lighter version
+                customBlueLight2: '#558b97', // Add this line for the second lighter version
+            },
+            keyframes: {
+                shake: {
+                    '0%, 100%': { transform: 'translateX(0)' },
+                    '50%': { transform: 'translateX(-5px)' },
+                },
+                fadeInLeft: {
+                    from: { opacity: '0', transform: 'translateX(-100%)' },
+                    to: { opacity: '1', transform: 'translateX(0)' },
+                },
+                fadeInRight: {
+                    from: { opacity: '0', transform: 'translateX(100%)' },
+                    to: { opacity: '1', transform: 'translateX(0)' },
+                },
+                fadeInUp: {
+                    from: { opacity: '0', transform: 'translateY(100%)' },
+                    to: { opacity: '1', transform: 'translateY(0)' },
+                },
+                fadeOut: {
+                    from: { opacity: '1' },
+                    to: { opacity: '0' },
+                },
+                fadeOutDown: {
+                    from: { opacity: '1', transform: 'translateY(0)' },
+                    to: { opacity: '0', transform: 'translateY(100%)' },
+                },
+                fadeOutLeft: {
+                    from: { opacity: '1', transform: 'translateX(0)' },
+                    to: { opacity: '0', transform: 'translateX(-100%)' },
+                },
+                fadeOutRight: {
+                    from: { opacity: '1', transform: 'translateX(0)' },
+                    to: { opacity: '0', transform: 'translateX(100%)' },
+                },
+                fadeOutUp: {
+                    from: { opacity: '1', transform: 'translateY(0)' },
+                    to: { opacity: '0', transform: 'translateY(-100%)' },
+                },
+                slideInDown: {
+                    from: { transform: 'translateY(-100%)' },
+                    to: { transform: 'translateY(0)' },
+                },
+                slideInLeft: {
+                    from: { transform: 'translateX(-100%)' },
+                    to: { transform: 'translateX(0)' },
+                },
+                slideInRight: {
+                    from: { transform: 'translateX(100%)' },
+                    to: { transform: 'translateX(0)' },
+                },
+                slideInUp: {
+                    from: { transform: 'translateY(100%)' },
+                    to: { transform: 'translateY(0)' },
+                },
+                slideOutDown: {
+                    from: { transform: 'translateY(0)' },
+                    to: { transform: 'translateY(100%)' },
+                },
+                slideOutLeft: {
+                    from: { transform: 'translateX(0)' },
+                    to: { transform: 'translateX(-100%)' },
+                },
+                slideOutRight: {
+                    from: { transform: 'translateX(0)' },
+                    to: { transform: 'translateX(100%)' },
+                },
+                slideOutUp: {
+                    from: { transform: 'translateY(0)' },
+                    to: { transform: 'translateY(-100%)' },
+                },
+                zoomIn: {
+                    from: { transform: 'scale(0)' },
+                    to: { transform: 'scale(1)' },
+                },
+                zoomOut: {
+                    from: { transform: 'scale(1)' },
+                    to: { transform: 'scale(0)' },
+                },
+                tada: {
+                    '0%, 100%': { transform: 'scale(1)' },
+                    '10%, 20%': { transform: 'scale(0.9) rotate(-3deg)' },
+                    '30%, 50%, 70%, 90%': { transform: 'scale(1.1) rotate(3deg)' },
+                    '40%, 60%, 80%': { transform: 'scale(1.1) rotate(-3deg)' },
+                },
+                spinnerGrow: {
+                    '0%': { transform: 'scale(0)' },
+                    '50%': { transform: 'scale(1)' },
+                    '100%': { transform: 'scale(0)' },
+                },
+                slideDown: {
+                    from: { transform: 'translateY(-100%)' },
+                    to: { transform: 'translateY(0)' },
+                },
+                slideLeft: {
+                    from: { transform: 'translateX(100%)' },
+                    to: { transform: 'translateX(0)' },
+                },
+                slideRight: {
+                    from: { transform: 'translateX(-100%)' },
+                    to: { transform: 'translateX(0)' },
+                },
+                slideUp: {
+                    from: { transform: 'translateY(100%)' },
+                    to: { transform: 'translateY(0)' },
+                },
+            },
+            animation: {
+                shake: 'shake 0.5s ease-in-out infinite',
+                bounce: 'bounce 1s infinite', // Ensure bounce animation is enabled
+                fadeInLeft: 'fadeInLeft 1s ease-in-out',
+                fadeInRight: 'fadeInRight 1s ease-in-out',
+                fadeInUp: 'fadeInUp 1s ease-in-out',
+                fadeOut: 'fadeOut 1s ease-in-out',
+                fadeOutDown: 'fadeOutDown 1s ease-in-out',
+                fadeOutLeft: 'fadeOutLeft 1s ease-in-out',
+                fadeOutRight: 'fadeOutRight 1s ease-in-out',
+                fadeOutUp: 'fadeOutUp 1s ease-in-out',
+                slideInDown: 'slideInDown 1s ease-in-out',
+                slideInLeft: 'slideInLeft 1s ease-in-out',
+                slideInRight: 'slideInRight 1s ease-in-out',
+                slideInUp: 'slideInUp 1s ease-in-out',
+                slideOutDown: 'slideOutDown 1s ease-in-out',
+                slideOutLeft: 'slideOutLeft 1s ease-in-out',
+                slideOutRight: 'slideOutRight 1s ease-in-out',
+                slideOutUp: 'slideOutUp 1s ease-in-out',
+                zoomIn: 'zoomIn 1s ease-in-out',
+                zoomOut: 'zoomOut 1s ease-in-out',
+                tada: 'tada 1s ease-in-out',
+                spinnerGrow: 'spinnerGrow 1s infinite ease-in-out',
+                slideDown: 'slideDown 1s ease-in-out',
+                slideLeft: 'slideLeft 1s ease-in-out',
+                slideRight: 'slideRight 1s ease-in-out',
+                slideUp: 'slideUp 1s ease-in-out',
+            },
         },
-      },
     },
-  },
-  variants: {
-    extend: {
-      animation: ['hover'], // Ensure hover variant is enabled for animations
+    variants: {
+        extend: {
+            animation: ['hover'], // Ensure hover variant is enabled for animations
+        },
     },
-  },
-  plugins: [],
+    plugins: [
+        require('tailwindcss-animate'),
+    ],
 };
